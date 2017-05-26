2f55442b78297b25cac3bf5b820ba04c1377f007
jixuduxing <jixuduxing@gmail.com>
2017-05-26 16:29:10 +0800

    1

--- weixin/views.py
+++ weixin/views.py
@@ -1,12 +1,18 @@
 import os
 from django.shortcuts import render
 from django.conf import settings
+from django.views.generic.base import View
 from django.http import HttpResponse
-
 
 def hello(request):
     # hostname = os.getenv('HOSTNAME', 'unknown')
-    return HttpResponse("Hello world ! ")
+    if request.method == 'GET':
+        rsp = request.GET.get('echostr', 'error')
+    else:
+        # message = wechat.get_message()
+        # rsp = wechat.response_text(u'消息类型: {}'.format(message.type))
+        return HttpResponse("Hello world ! ")
+    return HttpResponse(rsp)
     # return render(request, 'welcome/index.html', {
     #     'hostname': hostname,
     #     'database': database.info(),
