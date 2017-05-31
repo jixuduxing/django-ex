8914271a7c698e32b04ad3bbae1bf375f121f1b3
jixuduxing <jixuduxing@gmail.com>
2017-05-31 15:43:51 +0800

    1

--- weixin/views.py
+++ weixin/views.py
@@ -50,7 +50,6 @@ def hello(request):
         rsp = request.GET.get('echostr', 'error')
     else:
         # message = wechat.get_message()
-        # rsp = wechat.response_text(u'消息类型: {}'.format(message.type))
         return HttpResponse("Hello world ! ")
     return HttpResponse(rsp)
     return render(request, 'welcome/index.html', {
