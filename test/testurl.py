8bb698b708b8f55c16724028511118ae947ca5c8
jixuduxing <jixuduxing@gmail.com>
2017-06-05 17:15:22 +0800

    1

--- test/testurl.py
+++ test/testurl.py
@@ -3,6 +3,12 @@ import urllib2
 import cookielib
 from urllib2 import HTTPError
 import traceback
+
+from django.test import Client
+
+# c = Client()
+# response = c.post('/login/', {'username': 'john', 'password': 'smith'})
+# response.status_code
 try:
     cj = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
@@ -10,7 +16,10 @@ try:
     urllib2.install_opener(opener)
     req = urllib2.Request("http://django-ex-jixuduxing.1d35.starter-us-east-1.openshiftapps.com/weixin")
     req.add_data(urllib.urlencode({"username":"root","password":"ROOTXXOO"}))
-    req.add_header("Referer", "http://django-ex-jixuduxing.1d35.starter-us-east-1.openshiftapps.com")
+    req.add_header("Referer", "www.baidu.com")
+    req.add_header("Cookie", "csrftoken=HUQYHNYCbEetb96FqsCdhzfNSjAnQfW3")
+    print req
+
     print req.get_method()
     resp = urllib2.urlopen(req)
     print resp.read()
